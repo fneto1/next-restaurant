0e68f60c125e66c8883a51dee866b6d9b82a5bdb
fneto1 <fasneto2@gmail.com>
2023-09-02 21:37:10 -0300

    Criação de barra de notificações, navbar e footer

---
+++ src/components/Menu.tsx
@@ -0,0 +1,30 @@
+"use client";
+import Image from "next/image";
+import React, { useState } from "react";
+
+const Menu = () => {
+  const [open, setOpen] = useState(false);
+  return (
+    <div>
+      {!open ? (
+        <Image
+          src="/open.png"
+          alt=""
+          width={20}
+          height={20}
+          onClick={() => setOpen(true)}
+        />
+      ) : (
+        <Image
+          src="/close.png"
+          alt=""
+          width={20}
+          height={20}
+          onClick={() => setOpen(false)}
+        />
+      )}
+    </div>
+  );
+};
+
+export default Menu;
---
+++ src/components/Notification.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Notification = () => {
+  return (
+    <div className="h-12 bg-red-500 text-white px-4 flex items-center justify-center text-center text-sm md:text-base cursor-pointer">
+      Frete gratis para pedidos acima de R$ 50,00. Peça a sua refeição agora!
+    </div>
+  );
+};
+
+export default Notification;
