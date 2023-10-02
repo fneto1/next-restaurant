8d5216d5b70e295ca22a4d1b49459e8b30471525
fneto1 <fasneto2@gmail.com>
2023-10-01 21:22:54 -0300

    Front finalizado

--- src/app/orders/page.tsx
+++ src/app/orders/page.tsx
@@ -1,7 +1,44 @@
 import React from "react";
 
 const OrdersPage = () => {
-  return <div>OrdersPage</div>;
+  return (
+    <div className="p-4 lg:px-20 xl:px-40">
+      <table className=" w-full border-separate border-spacing-3">
+        <thead className="">
+          <tr className="text-left">
+            <th className="hidden md:block">Order id</th>
+            <th>Date</th>
+            <th>Price</th>
+            <th className="hidden md:block">Products</th>
+            <th>Status</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr className="text-sm md:text-base bg-red-50">
+            <td className="hidden md:block py-6 px-1">01</td>
+            <td className="py-6 px-1">01/10/2023</td>
+            <td className="py-6 px-1">R$ 100,00</td>
+            <td className="hidden md:block py-6 px-1">Pizza</td>
+            <td className="py-6 px-1">Preparando</td>
+          </tr>
+          <tr className="text-sm md:text-base odd:bg-gray-100">
+            <td className="hidden md:block py-6 px-1">01</td>
+            <td className="py-6 px-1">01/10/2023</td>
+            <td className="py-6 px-1">R$ 100,00</td>
+            <td className="hidden md:block py-6 px-1">Pizza</td>
+            <td className="py-6 px-1">Preparando</td>
+          </tr>
+          <tr className="text-sm md:text-base odd:bg-gray-100">
+            <td className="hidden md:block py-6 px-1">01</td>
+            <td className="py-6 px-1">01/10/2023</td>
+            <td className="py-6 px-1">R$ 100,00</td>
+            <td className="hidden md:block py-6 px-1">Pizza</td>
+            <td className="py-6 px-1">Preparando</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
 };
 
 export default OrdersPage;
